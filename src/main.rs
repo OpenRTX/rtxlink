f77433223e7a41c3d631c132af002aea10812621
Federico Amedeo Izzo <federico@izzo.pro>
2022-03-15 22:26:24 +0100

    Move xmodem transfer to thread, check output file size in main()

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::env;
-use std::fs::File;
+use std::fs::{File, metadata};
 use std::time::Duration;
+use std::thread;
+use thread_control::*;
 use ymodem::xmodem;
+
+const OUTPUT_PATH: &str = "./flash_dump.bin";
 
 fn main() {
 
@@ -11,14 +15,31 @@ fn main() {
     let mut port = serialport::new(tty_dev, 115_200)
         .timeout(Duration::from_millis(10))
         .open().expect("Failed to open serial port");
-    let mut output_file = File::create("./flash_dump.bin")
+    let mut output_file = File::create(OUTPUT_PATH)
         .expect("Failed to open output file");
 
     // Create xmodem with 1K blocks
     let mut xmodem = xmodem::Xmodem::new();
     xmodem.block_length = xmodem::BlockLength::OneK;
 
-    // Receive full flash copy
-    xmodem.recv(&mut port, &mut output_file, xmodem::Checksum::CRC16)
-          .expect("Failed to receive xmodem transfer"); 
+    // Workaround for missing handle.is_running()
+    // https://github.com/DenisKolodin/thread-control
+    let (flag, control) = make_pair();
+    let handle = thread::spawn(move || {
+        if !flag.alive() { return; }
+        println!("XMODEM transfer started");
+        xmodem.recv(&mut port, &mut output_file, xmodem::Checksum::CRC16)
+              .expect("Failed to receive xmodem transfer");
+        println!("XMODEM transfer finished");
+    });
+
+    // handle.is_running() is not available yet, use it when it is released
+    // https://github.com/rust-lang/rust/issues/90470
+    while !control.is_done() {
+        let output_size = metadata(OUTPUT_PATH).unwrap().len();
+        println!("{} size: {} Bytes", OUTPUT_PATH, output_size);
+        thread::sleep(Duration::from_millis(1000));
+    }
+    // Wait for xmodem thread to finish
+    handle.join().unwrap();
 }
