e4bc6cdf998354d62aa97d8195ae1163dd19257b
Federico Amedeo Izzo <federico@izzo.pro>
2022-03-28 21:10:49 +0200

    Add draft flash command

--- src/main.rs
+++ src/main.rs
@@ -25,9 +25,8 @@ fn main() {
     let command = args[1].clone();
     let serial_port = args[2].clone();
 
-    if command == "dump" {
-        dump(serial_port);
-    }
+    if command == "dump" { dump(serial_port); }
+    else if command == "flash" { flash(serial_port); }
 }
 
 fn dump(serial_port: String) {
@@ -57,6 +56,38 @@ fn dump(serial_port: String) {
     while !control.is_done() {
         let output_size = metadata(OUTPUT_PATH).unwrap().len();
         println!("{} size: {} Bytes", OUTPUT_PATH, output_size);
+        thread::sleep(Duration::from_millis(1000));
+    }
+    // Wait for xmodem thread to finish
+    handle.join().unwrap();
+}
+
+fn flash(serial_port: String) {
+    let mut port = serialport::new(serial_port, 115_200)
+        .timeout(Duration::from_millis(10))
+        .open().expect("Failed to open serial port");
+    let mut output_file = File::open(OUTPUT_PATH)
+        .expect("Failed to open input file");
+
+    // Create xmodem with 1K blocks
+    let mut xmodem = xmodem::Xmodem::new();
+    xmodem.block_length = xmodem::BlockLength::OneK;
+
+    // Workaround for missing handle.is_running()
+    // https://github.com/DenisKolodin/thread-control
+    let (flag, control) = make_pair();
+    let handle = thread::spawn(move || {
+        if !flag.alive() { return; }
+        println!("XMODEM transfer started");
+        xmodem.send(&mut port, &mut output_file)
+              .expect("Failed to send xmodem transfer");
+        println!("XMODEM transfer finished");
+    });
+
+    // handle.is_running() is not available yet, use it when it is released
+    // https://github.com/rust-lang/rust/issues/90470
+    while !control.is_done() {
+        println!("Sending data...");
         thread::sleep(Duration::from_millis(1000));
     }
     // Wait for xmodem thread to finish
